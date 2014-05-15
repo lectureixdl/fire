51615ba8354cea7aa1d12b11191f9a1f0ad21258
gim8912 <gim8912@gmail.com>
2014-05-15 20:58:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -37,13 +37,18 @@ $(document).ready(function(){
 
 	var fan = function(){
 		if(temp_y === 7){
-	           $('.fan').addClass('fan');
+	           $('.fan').togleClass('fan');
+	           $('.fan').removeClass('fan2');
+	           $('.fan').removeClass('fan3');
 	        }
 	        else if(temp_y === 4){
 	           $('.fan').addClass('fan2');
+	           $('.fan').removeClass("fan3");
+
 	        }
 	        else if (temp_y === 0){
 	           $('.fan').addClass('fan3');
+	           $('.fan').removeClass("fan2");
 	        }
 	}
 
